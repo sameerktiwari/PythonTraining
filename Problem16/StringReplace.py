269ec510be038d08039f16404b1739037440eb18
Tiwari <sameer.tiwari@capgemini.com>
2017-11-29 19:27:41 +0530

    Assignment 16 done

---
+++ Problem16/StringReplace.py
@@ -0,0 +1,7 @@
+str="Do dogs eat cats, or does your cat eat a mouse?"
+str=str.replace("dog","cat")
+print(str)
+str=str.replace("cat","mouse")
+print(str)
+str=str.replace("mouse","dog")
+print(str)
